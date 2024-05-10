9c06e4901d902b5da7274dc1e8e20a03e9f1a844
Giovanne Bressam Gaspareto <giovannebressam@gmail.com>
2024-05-09 22:27:19 -0300

    Added project

---
+++ YOSDA-SignIn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YOSDA-SignIn
+//
+//  Created by Giovanne Bressam on 09/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YOSDA-SignIn/YOSDA_SignInApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOSDA_SignInApp.swift
+//  YOSDA-SignIn
+//
+//  Created by Giovanne Bressam on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct YOSDA_SignInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
